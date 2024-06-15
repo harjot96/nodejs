abd7389c6505d9072d51570c3def682177e20f6a
Harjot Singh <harjot@weexcel.com>
2024-06-15 13:39:29 +0530

    latest code

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,14 @@
-const express=require('express');
+const express = require('express');
 const path = require('path');
 const routes = express.Router();
-const adminData=require('./admin');
-routes.get('/',(req, res, next) => {
-    prodcut=adminData.products;
+const adminData = require('./admin');
+routes.get('/', (req, res, next) => {
+    const prodcuts = adminData.products;
+    console.log('====================================');
+    console.log(adminData.products);
+    console.log('====================================');
+    prodcut = adminData.products;
     // res.sendFile(path.join(__dirname,'../','views','shop.html'));
-    res.render('shop');
+    res.render('shop', { prods: prodcuts, docTitle: 'shop' });
 });
 module.exports = routes; 
